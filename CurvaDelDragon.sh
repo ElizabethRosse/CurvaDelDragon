abc0273edbfef84b7489f1bd975d81051d63f992
Elizabeth Muñiz <eli_mure@ciencias.unam.mx>
2015-03-07 21:37:52 -0600

    Create CurvaDelDragon.sh

---
+++ CurvaDelDragon.sh
@@ -0,0 +1,10 @@
+#!/bin/bash                                                                                                                                           
+
+clear
+echo "Escribe las iteraciones para las que quieres calcular los giros"
+echo "Cada línea contiene un caso de prueba con un número entero N (0 <= N <= 15)."
+echo "Para finalizar la entrada escribe -1"
+echo
+
+chmod +x Giros.py
+python3 /home/elizabeth/Modelado/CurvaDelDragon/Giros.py
